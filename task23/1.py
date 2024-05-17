7f227ea296c5c444bc0d22fb427de47b24ad0cca
r_aitov <r_aitov@mail.ru>
2024-05-17 17:02:15 +0300

    task 23

--- task23/1.py
+++ task23/1.py
@@ -1,10 +1,13 @@
-def f(s, e):
+def f(s, e, h):
     if s > e:
         return 0
     if s == e:
-        return 1
-    m = [f(s + 1, e), f(s * 2, e), f(s * 3, e)]
+        if h[-2] == '2' or h[-2] == '3':
+            return 1
+        else:
+            return 0
+    m = [f(s + 1, e, h + '1'), f(s * 2, e, h + '2'), f(s * 3, e, h + '3')]
     return sum(m)
 
 
-print(f(1, 15))
+print(f(1, 15, ''))
---
+++ task25/t.py
@@ -0,0 +1,5 @@
+from fnmatch import fnmatch
+
+for n in range(..):
+    if fnmatch(..):
+        print(n, n // ..)
